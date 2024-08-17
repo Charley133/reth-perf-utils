765b6fec6148b2afe88a4700d80e6c394933346d
Charley133 <161747866+Charley133@users.noreply.github.com>
2024-08-17 08:32:53 +0200

    macros.rs - update 17.08.2024
    Fixed the incorrect usage of checked_cycles_since, replacing it with checked_duration_since for accurate time calculations.
    Enhanced error handling for duration calculation by defaulting to 0 on overflow.
    Improved naming consistency across macros for better readability and maintainability.
    Ensured the impl_write_macro is only compiled when the enable_execution_duration_record feature is enabled, making the codebase more efficient and modular.

--- perf-metrics/src/metrics/macros.rs
+++ perf-metrics/src/metrics/macros.rs
@@ -1,10 +1,12 @@
+use std::time::Instant;
+
 #[allow(unused_macros)]
 macro_rules! define_record_with_elapsed_time_function {
     ($name:ident, $field:ident, $time_counter:ident) => {
         pub(super) fn $name(&mut self) -> Instant {
             // Calculate duration and reset time_counter.
             let now = Instant::now();
-            let cycles = now.checked_cycles_since(self.$time_counter).unwrap_or(0);
+            let cycles = now.checked_duration_since(self.$time_counter).unwrap_or_default();
             self.$time_counter = now;
             // Record duration.
             self.$field = self.$field.checked_add(cycles).expect("overflow");
@@ -19,7 +21,7 @@ macro_rules! define_record_time_function {
         pub(super) fn $name(&mut self) {
             // Calculate duration.
             let cycles = Instant::now()
-                .checked_cycles_since(self.$time_counter)
+                .checked_duration_since(self.$time_counter)
                 .expect("overflow");
             // Record duration.
             self.$field = self.$field.checked_add(cycles).expect("overflow");
